f419f63ebd0b2976746d667f3f01d5534e4ced0a
Elena Vilchik <elena.vilchik@sonarsource.com>
2017-05-16 18:04:12 +0200

    add rule no-ignored-return RSPEC-2201

---
+++ src/rules/NoIgnoredReturnRule.ts
@@ -0,0 +1,232 @@
+import * as Lint from "tslint";
+import * as ts from "typescript";
+import { SonarRuleMetaData } from "../sonarRule";
+
+// https://jira.sonarsource.com/browse/RSPEC-2201
+
+export class Rule extends Lint.Rules.TypedRule {
+
+    public static metadata: SonarRuleMetaData = {
+        description: "Return values should not be ignored when function calls don't have any side effects",
+        options: null,
+        optionsDescription: "",
+        rationale: Lint.Utils.dedent`When the call to a function doesn't have any side effect,
+            what is the point of making the call if the results are ignored?
+            In such cases, either the function call is useless and should be dropped,
+            or the source code doesn't behave as expected.`,
+        rspecKey: "RSPEC-2201",
+        ruleName: "no-ignored-return",
+        type: "functionality",
+        typescriptOnly: false,
+    };
+
+    public applyWithProgram(sourceFile: ts.SourceFile, program: ts.Program): Lint.RuleFailure[] {
+        return this.applyWithWalker(new Walker(sourceFile, this.getOptions(), program));
+    }
+}
+
+class Walker extends Lint.ProgramAwareRuleWalker {
+    private METHODS_WITHOUT_SIDE_EFFECTS: { [index: string]: Set<string> } = {
+        array: new Set([
+            "concat",
+            "includes",
+            "join",
+            "slice",
+            "indexOf",
+            "lastIndexOf",
+            "entries",
+            "every",
+            "some",
+            "filter",
+            "findIndex",
+            "keys",
+            "map",
+            "values",
+            "find",
+            "reduce",
+            "reduceRight",
+            "toString",
+            "toLocaleString",
+        ]),
+        date: new Set([
+            "getDate",
+            "getDay",
+            "getFullYear",
+            "getHours",
+            "getMilliseconds",
+            "getMinutes",
+            "getMonth",
+            "getSeconds",
+            "getTime",
+            "getTimezoneOffset",
+            "getUTCDate",
+            "getUTCDay",
+            "getUTCFullYear",
+            "getUTCHours",
+            "getUTCMilliseconds",
+            "getUTCMinutes",
+            "getUTCMonth",
+            "getUTCSeconds",
+            "getYear",
+            "toDateString",
+            "toISOString",
+            "toJSON",
+            "toGMTString",
+            "toLocaleDateString",
+            "toLocaleTimeString",
+            "toTimeString",
+            "toUTCString",
+            "toString",
+            "toLocaleString",
+        ]),
+        math: new Set([
+            "abs",
+            "E",
+            "LN2",
+            "LN10",
+            "LOG2E",
+            "LOG10E",
+            "PI",
+            "SQRT1_2",
+            "SQRT2",
+            "abs",
+            "acos",
+            "acosh",
+            "asin",
+            "asinh",
+            "atan",
+            "atanh",
+            "atan2",
+            "cbrt",
+            "ceil",
+            "clz32",
+            "cos",
+            "cosh",
+            "exp",
+            "expm1",
+            "floor",
+            "fround",
+            "hypot",
+            "imul",
+            "log",
+            "log1p",
+            "log10",
+            "log2",
+            "max",
+            "min",
+            "pow",
+            "random",
+            "round",
+            "sign",
+            "sin",
+            "sinh",
+            "sqrt",
+            "tan",
+            "tanh",
+            "trunc",
+        ]),
+        number: new Set([
+            "toExponential",
+            "toFixed",
+            "toPrecision",
+            "toLocaleString",
+            "toString",
+        ]),
+        regexp: new Set([
+            "test",
+            "toString",
+        ]),
+        string: new Set([
+            "charAt",
+            "charCodeAt",
+            "codePointAt",
+            "concat",
+            "includes",
+            "endsWith",
+            "indexOf",
+            "lastIndexOf",
+            "localeCompare",
+            "match",
+            "normalize",
+            "padEnd",
+            "padStart",
+            "repeat",
+            "replace",
+            "search",
+            "slice",
+            "split",
+            "startsWith",
+            "substr",
+            "substring",
+            "toLocaleLowerCase",
+            "toLocaleUpperCase",
+            "toLowerCase",
+            "toUpperCase",
+            "trim",
+            "length",
+            "toString",
+            "valueOf",
+
+            // HTML wrapper methods
+            "anchor",
+            "big",
+            "blink",
+            "bold",
+            "fixed",
+            "fontcolor",
+            "fontsize",
+            "italics",
+            "link",
+            "small",
+            "strike",
+            "sub",
+            "sup",
+        ]),
+    };
+
+    public visitCallExpression(node: ts.CallExpression) {
+        if (node.expression.kind === ts.SyntaxKind.PropertyAccessExpression
+            && node.parent && node.parent.kind === ts.SyntaxKind.ExpressionStatement) {
+
+            const propertyAccess = node.expression as ts.PropertyAccessExpression;
+            const methodName = propertyAccess.name.text;
+            const object = propertyAccess.expression;
+            const objectType = this.getTypeChecker().getTypeAtLocation(object);
+
+            if (this.methodWithNoSideEffect(objectType, methodName)) {
+                const msg = `The return value of "${methodName}" must be used.`;
+                this.addFailure(this.createFailure(node.getStart(), node.getWidth(), msg));
+            }
+        }
+
+        super.visitCallExpression(node);
+    }
+
+    private methodWithNoSideEffect(objectType: ts.Type, methodName: string): boolean {
+        const typeAsString = this.typeToString(objectType);
+        if (typeAsString !== null) {
+            const methods = this.METHODS_WITHOUT_SIDE_EFFECTS[typeAsString];
+            return methods && methods.has(methodName);
+        }
+        return false;
+    }
+
+    private typeToString(type: ts.Type): string | null {
+        const baseType = this.getTypeChecker().getBaseTypeOfLiteralType(type);
+        const typeAsString = this.getTypeChecker().typeToString(baseType);
+        if (typeAsString === "number" || typeAsString === "string") {
+            return typeAsString;
+        }
+
+        const name = baseType.getSymbol().getName();
+        switch (name) {
+            case "Array":
+            case "Date":
+            case "Math":
+            case "RegExp":
+                return name.toLowerCase();
+            default:
+                return null;
+        }
+    }
+}
---
+++ tests/rules/NoIgnoredReturnRule/NoIgnoredReturnRule.lint.ts
@@ -0,0 +1,65 @@
+// tslint:disable
+
+function methodsOnMath() {
+  let x = -42;
+  Math.abs(x);
+//^^^^^^^^^^^    {{The return value of "abs" must be used.}}
+}
+
+function methodsOnDates() {
+  var date = new Date();
+  date.getDay();
+//^^^^^^^^^^^^^    {{The return value of "getDay" must be used.}}
+}
+
+function methodsOnArray(arr1: any[]) {
+  let arr = [1, 2, 3];
+  arr.slice(0, 2);
+//^^^^^^^^^^^^^^^    {{The return value of "slice" must be used.}}
+
+  arr1.join(",");
+//^^^^^^^^^^^^^^    {{The return value of "join" must be used.}}
+}
+
+function methodsOnString() {
+  let x = "abc";
+
+  x.concat("abc");
+//^^^^^^^^^^^^^^^    {{The return value of "concat" must be used.}}
+
+  "abc".concat("bcd").charCodeAt(2);
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^    {{The return value of "charCodeAt" must be used.}}
+
+  "abc".concat("bcd");
+//^^^^^^^^^^^^^^^^^^^    {{The return value of "concat" must be used.}}
+}
+
+function methodsOnNumbers() {
+  var num = 43 * 53;
+  num.toExponential();
+//^^^^^^^^^^^^^^^^^^^    {{The return value of "toExponential" must be used.}}
+}
+
+function methodsOnRegexp() {
+  var regexp = /abc/;
+  regexp.test("my string");
+//^^^^^^^^^^^^^^^^^^^^^^^^    {{The return value of "test" must be used.}}
+}
+
+function returnIsNotIgnored() {
+  var x = "abc".concat("bcd");
+
+  if ([1, 2, 3].lastIndexOf(42)) {
+    return true;
+  }
+}
+
+function noSupportForUserTypes() {
+  class A {
+    methodWithoutSideEffect() {
+      return 42;
+    }
+  }
+
+  (new A()).methodWithoutSideEffect(); // OK
+}
