c951a60dab80431b6219c522938428a7ea2f4e0e
Elena Vilchik <elena.vilchik@sonarsource.com>
2017-05-16 18:04:12 +0200

    add rule no-ignored-return RSPEC-2201

---
+++ tests/rules/NoIgnoredReturnRule/NoIgnoredReturnRule.test.ts
@@ -0,0 +1,7 @@
+import { Rule } from "../../../src/rules/NoIgnoredReturnRule";
+import runRule from "../../runRule";
+
+it("test", () => {
+  const result = runRule(Rule, __filename);
+  expect(result.actualErrors).toEqual(result.expectedErrors);
+});
