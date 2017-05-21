970329f39c444ef926f46c759062ca507594f070
root <hbaloo28@gmail.com>
2017-05-21 19:57:23 +0200

    Update test descriptions
    On naming:
    Update and align test descriptions to "unit test" and "ruling test". The terms are still not very clear. Ruling tests could be called simply integration. Unit could be just renamed to lint testing. The term "lint" is already in use in the data file name and throughout runRule.ts.
    
    On code organization:
    Currently the 2 existing test.ts files are identical, expect for the Rule file name. In order to reduce code duplications, and unwanted differences like this commit tries to fix, separate test.ts files could be eliminated, and the Rule file names used as input for a parametrized testing setup.

--- tests/rules/NoIdenticalExpressionsRule/NoIdenticalExpressionsRule.test.ts
+++ tests/rules/NoIdenticalExpressionsRule/NoIdenticalExpressionsRule.test.ts
@@ -1,11 +1,11 @@
 import { Rule } from "../../../src/rules/NoIdenticalExpressionsRule";
 import { runRule, runRuleOnRuling } from "../../runRule";
 
-it("raises error", () => {
+it("unit test", () => {
   const { actualErrors, expectedErrors } = runRule(Rule, __filename);
   expect(actualErrors).toEqual(expectedErrors);
 });
 
-it("ruling", () => {
+it("ruling test", () => {
   expect(runRuleOnRuling(Rule)).toMatchSnapshot();
 });
