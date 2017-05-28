bec3c09dc59815c4ec4e68eda90e389422a658fd
Carlo Bottiglieri <carlo.bottiglieri@gmail.com>
2017-05-28 19:35:00 +0200

    move CfgBuilder to its own file

--- src/cfg/cfg.ts
+++ src/cfg/cfg.ts
@@ -1,213 +1,14 @@
 import { flatten, uniqBy } from "lodash";
 import * as ts from "typescript";
+import { CfgBuilder } from "./builder";
 
 const { SyntaxKind } = ts;
-
-// create end
-// create current
-// throwTargets.push(end)?
-// build:
-//   - reverse list of statements
-//   -
-// ...
-// start = current block
-// removeEmptyBlocks?
-// add end
-
-class CfgBuilder {
-
-  private blockCounter = 0;
-  private end = new CfgEndBlock();
-
-  public build(statements: ts.NodeArray<ts.Statement>): ControlFlowGraph {
-    const current = this.createBlock();
-    current.addSuccessor(this.end);
-    const start = this.buildStatements(current, statements);
-
-    const graph = new ControlFlowGraph();
-    graph.addStart(start);
-    graph.finalize();
-    start.addElement(({ getText() { return "START"; } } as ts.Expression));
-    return graph;
-  }
-
-  private buildStatements(currentBlock: CfgBlock, topDownStatements: ts.Statement[]): CfgBlock {
-    let current = currentBlock;
-    const statements = [...topDownStatements].reverse();
-
-    statements.forEach(statement => {
-      switch (statement.kind) {
-        case SyntaxKind.Block: {
-          const block = statement as ts.Block;
-          current = this.buildStatements(current, block.statements);
-          break;
-        }
-        case SyntaxKind.ExpressionStatement:
-          current = this.buildExpression(current, (statement as ts.ExpressionStatement).expression);
-          break;
-        case SyntaxKind.IfStatement: {
-          const ifStatement = statement as ts.IfStatement;
-          let whenFalse;
-          if (ifStatement.elseStatement) {
-            whenFalse = this.buildStatements(this.createPredecessorBlock(current), [ifStatement.elseStatement]);
-          } else {
-            whenFalse = current;
-          }
-          const whenTrue = this.buildStatements(this.createPredecessorBlock(current), [ifStatement.thenStatement]);
-          current = this.buildExpression(
-            new CfgBranchingBlock("if (" + ifStatement.expression.getText() + ")", whenTrue, whenFalse),
-            ifStatement.expression,
-          );
-          break;
-        }
-        case SyntaxKind.ForStatement: {
-          const forLoop = statement as ts.ForStatement;
-          const loopBottom = new CfgBlock();
-          let lastLoopStatementBlock = loopBottom;
-          if (forLoop.incrementor) {
-            lastLoopStatementBlock = this.buildExpression(lastLoopStatementBlock, forLoop.incrementor);
-          }
-          const firstLoopStatementBlock = this.buildStatements(lastLoopStatementBlock, [forLoop.statement]);
-          let loopRoot: CfgBlock;
-          if (forLoop.condition) {
-            loopRoot = this.buildExpression(
-              new CfgBranchingBlock(this.forLoopLabel(forLoop), firstLoopStatementBlock, current), forLoop.condition);
-          } else {
-            loopRoot = this.createPredecessorBlock(firstLoopStatementBlock);
-          }
-          let loopStart = loopRoot;
-          if (forLoop.initializer) {
-            loopStart = this.buildExpression(this.createPredecessorBlock(loopRoot), forLoop.initializer);
-          }
-          loopBottom.addSuccessor(loopRoot);
-          current = loopStart;
-          break;
-        }
-        case SyntaxKind.WhileStatement: {
-          const whileLoop = statement as ts.WhileStatement;
-          const loopBottom = new CfgBlock();
-          const firstLoopStatementBlock = this.buildStatements(loopBottom, [whileLoop.statement]);
-          const loopStart = this.buildExpression(
-            new CfgBranchingBlock("while(" + whileLoop.expression.getText() + ")", firstLoopStatementBlock, current),
-            whileLoop.expression,
-          );
-          loopBottom.addSuccessor(loopStart);
-          current = loopStart;
-          break;
-        }
-        case SyntaxKind.DoStatement: {
-          const doWhileLoop = statement as ts.DoStatement;
-          const doBlockEnd = new CfgBlock();
-          const doBlockStart = this.buildStatements(doBlockEnd, [doWhileLoop.statement]);
-          const whileBlockEnd = new CfgBranchingBlock("while(" + doWhileLoop.expression.getText() + ")", doBlockStart, current);
-          const whileStartBlock = this.buildExpression(whileBlockEnd, doWhileLoop.expression);
-          doBlockEnd.addSuccessor(whileStartBlock);
-          current = doBlockStart;
-          break;
-        }
-        case SyntaxKind.SwitchStatement: {
-          const switchStatement = statement as ts.SwitchStatement;
-          const clauses = [...switchStatement.caseBlock.clauses].reverse();
-          const clausesBlocks = clauses.map(clause => {
-            current = this.buildStatements(this.createPredecessorBlock(current), clause.statements);
-            return current;
-          });
-          const switchEndBlock = new CfgBlock();
-          clausesBlocks.forEach(clauseBlock => switchEndBlock.addSuccessor(clauseBlock));
-          current = this.buildExpression(switchEndBlock, switchStatement.expression);
-          break;
-        }
-        case SyntaxKind.ReturnStatement: {
-          const returnStatement = statement as ts.ReturnStatement;
-          if (returnStatement.expression) {
-            current = this.buildExpression(this.createPredecessorBlock(this.end), returnStatement.expression);
-          } else {
-            current = this.createPredecessorBlock(this.end);
-          }
-          break;
-        }
-        default:
-          throw new Error("Unknown statement: " + SyntaxKind[statement.kind]);
-      }
-    });
-
-    return current;
-  }
-
-  private buildExpression(current: CfgBlock, expression: ts.Expression | ts.VariableDeclarationList): CfgBlock {
-    switch (expression.kind) {
-      case SyntaxKind.CallExpression: {
-        current.addElement(expression);
-        const callExpression = expression as ts.CallExpression;
-        let newCurrent = current;
-        [...callExpression.arguments].reverse().forEach(arg => {
-          newCurrent = this.buildExpression(newCurrent, arg);
-        });
-        newCurrent = this.buildExpression(newCurrent, callExpression.expression);
-
-        return newCurrent;
-      }
-      case SyntaxKind.ConditionalExpression: {
-        const conditionalExpression = expression as ts.ConditionalExpression;
-        const whenFalse = this.buildExpression(this.createPredecessorBlock(current), conditionalExpression.whenFalse);
-        const whenTrue = this.buildExpression(this.createPredecessorBlock(current), conditionalExpression.whenTrue);
-        return this.buildExpression(
-          new CfgBranchingBlock(expression.getText(), whenTrue, whenFalse), conditionalExpression.condition,
-        );
-      }
-      case SyntaxKind.BinaryExpression: {
-        const binaryExpression = expression as ts.BinaryExpression;
-        current.addElement(expression);
-        if (binaryExpression.operatorToken.kind === SyntaxKind.EqualsToken) {
-          const right = this.buildExpression(current, binaryExpression.right);
-          return this.buildExpression(right, binaryExpression.left);
-        }
-      }
-      case SyntaxKind.TrueKeyword:
-      case SyntaxKind.FalseKeyword:
-      case SyntaxKind.NumericLiteral:
-      case SyntaxKind.StringLiteral:
-      case SyntaxKind.Identifier:
-        current.addElement(expression);
-        return current;
-      default:
-        throw new Error("Unknown expression: " + SyntaxKind[expression.kind]);
-    }
-  }
-
-  private createBlock(): CfgBlock {
-    return new CfgBlock();
-  }
-
-  private createPredecessorBlock(successor: CfgBlock) {
-    const predecessor = new CfgBlock();
-    predecessor.addSuccessor(successor);
-    return predecessor;
-  }
-
-  private forLoopLabel(forLoop: ts.ForStatement) {
-    return "for(" +
-      textOrEmpty(forLoop.initializer) +
-      "\;" + textOrEmpty(forLoop.condition) +
-      "\;" + textOrEmpty(forLoop.incrementor) +
-      ")";
-
-    function textOrEmpty(node?: ts.Node): string {
-      if (node) return node.getText();
-      return "";
-    }
-  }
-}
 
 export class ControlFlowGraph {
   private start: CfgBlock;
 
   public static fromSource(statements: ts.NodeArray<ts.Statement>): ControlFlowGraph {
     return new CfgBuilder().build(statements);
-  }
-
-  public addStart(start: CfgBlock) {
-    this.start = start;
   }
 
   public getBlocks(): CfgBlock[] {
@@ -221,6 +22,10 @@ export class ControlFlowGraph {
       graphBlocks.push(block);
       block.getSuccessors().forEach((successor, i) => collectBlocks(successor, baseId + "\." + (i + 1)));
     }
+  }
+
+  public addStart(start: CfgBlock) {
+    this.start = start;
   }
 
   public finalize() {
