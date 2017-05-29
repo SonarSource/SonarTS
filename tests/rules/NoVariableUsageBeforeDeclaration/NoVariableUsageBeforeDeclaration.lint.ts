e781921959be715b2de0854b80a78fb2454165cc
Carlo Bottiglieri <carlo.bottiglieri@gmail.com>
2017-05-29 14:30:27 +0200

    test improvement

--- tests/rules/NoVariableUsageBeforeDeclaration/NoVariableUsageBeforeDeclaration.lint.ts
+++ tests/rules/NoVariableUsageBeforeDeclaration/NoVariableUsageBeforeDeclaration.lint.ts
@@ -2,48 +2,70 @@
 
 let foo, es6;
 
-function fun1(x) {
-  foo(x);            // OK
+function parameterUsages() {
+
+  function someFunction(x) {
+    foo(x);            // OK
+  }
+
+  foo(x);
+//    ^                  {{Move the declaration of "x" before this usage.}}
+  var x = 1;
+
 }
 
-foo(x);
-//  ^                  {{Move the declaration of "x" before this usage.}}
-var x = 1;
+function usagesFromWithinFunctions() {
 
-function fun2() {
-  foo(y);
-//    ^                {{Move the declaration of "y" before this usage.}}
-}
-var y = 1;
+  function someFunction() {
+    foo(y);
+//      ^                {{Move the declaration of "y" before this usage.}}
+  }
+  var y = 1;
 
-function* fun3() {
-  foo(z);
-//    ^                {{Move the declaration of "z" before this usage.}}
-}
-var z = 1;
+  function* generatorFunction() {
+    foo(z);
+//      ^                {{Move the declaration of "z" before this usage.}}
+  }
+  var z = 1;
 
-var fun4 = function () {
-  fun4();
-}
+  var recursive = function () {
+    recursive();
+  }
 
-var f = () => {
-  foo(c);
-//    ^                {{Move the declaration of "c" before this usage.}}
-}
-var c;
-
-for (e in es6) {}
-//   ^                 {{Move the declaration of "e" before this usage.}}
-var e;
-
-const bar = 3;
-
-for (var v in foo) {
-  foo[v] = 1;
 }
 
-function fun5() {
+function usageWithinArrowFunction() {
+  var f = () => {
+    foo(c);
+  //    ^                {{Move the declaration of "c" before this usage.}}
+  }
+  var c;
+}
+
+function usagesInLoops() {
+
+  for (e in es6) {}
+//     ^                 {{Move the declaration of "e" before this usage.}}
+  var e;
+
+  const bar = 3;
+
+  for (var v in foo) {
+    foo[v] = 1;
+  }
+
+}
+
+
+function ignoreAlreadyDeclaredVariable() {
   var i = 5;
   i++;
   var i = 6;
+}
+
+function multipleUsages() {
+  x = 0;
+//^                    {{Move the declaration of "x" before this usage.}}
+  x = 1;
+  var x;
 }
