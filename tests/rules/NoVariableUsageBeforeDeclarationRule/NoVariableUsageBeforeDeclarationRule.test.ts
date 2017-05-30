e9add1eac74ec571346513d2ee6b2db19c1ebdcc
Elena Vilchik <elena.vilchik@sonarsource.com>
2017-05-30 17:25:19 +0200

    fix rule name

--- tests/rules/NoVariableUsageBeforeDeclarationRule/NoVariableUsageBeforeDeclarationRule.test.ts
+++ tests/rules/NoVariableUsageBeforeDeclarationRule/NoVariableUsageBeforeDeclarationRule.test.ts
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-import { Rule } from "../../../src/rules/NoVariableUsageBeforeDeclaration";
+import { Rule } from "../../../src/rules/NoVariableUsageBeforeDeclarationRule";
 import runRule from "../../runRule";
 
 it("raises error", () => {
