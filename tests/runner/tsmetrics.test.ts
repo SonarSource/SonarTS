d7ae81349dac485ff118a7ba18aa4f37cad9f435
Elena Vilchik <elena.vilchik@sonarsource.com>
2017-07-19 11:55:21 +0200

    rename 'sonar' to 'tsmetrics' and make it executable

--- tests/runner/tsmetrics.test.ts
+++ tests/runner/tsmetrics.test.ts
@@ -18,7 +18,7 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 import * as ts from "typescript";
-import { processRequest } from "../../src/runner/sonar";
+import { processRequest } from "../../src/runner/tsmetrics";
 import { parseString } from "../../src/utils/parser";
 
 it("should process input", () => {
