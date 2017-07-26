ca09c159c23eaaac08ac29b93b9ba7f2c041291f
Stas Vilchik <stas.vilchik@sonarsource.com>
2017-07-26 14:44:59 +0200

    add no-duplicated-branches rule

---
+++ tests/rules/noDuplicatedBranchesRule/noDuplicatedBranchesRule.lint.ts
@@ -0,0 +1,171 @@
+// tslint:disable
+
+let a, b, first, second, third;
+
+/*
+ * NOK IF
+ */
+
+if (a) {
+  first();
+  second();
+} else {
+  // [12:7-16:1] {{This branch's code block is the same as the block for the branch on line 9.}}
+  first();
+  second();
+}
+
+if (a) {
+  first();
+  second();
+} else if (b) {
+  // [21:14-25:1] {{This branch's code block is the same as the block for the branch on line 18.}}
+  first();
+  second();
+}
+
+if (a) {
+  first();
+  second();
+} else if (b) {
+  second();
+  third();
+} else {
+  // [33:7-37:1] {{This branch's code block is the same as the block for the branch on line 27.}}
+  first();
+  second();
+}
+
+/*
+ * OK IF
+ */
+
+if (a) {
+  first(`const`);
+} else {
+  first(`var`);
+}
+
+// ok, small branches
+if (a) {
+  first();
+} else {
+  first();
+}
+
+if (a) {
+  first();
+  first();
+} else {
+  second();
+  second();
+}
+
+if (a) {
+  first();
+  second();
+} else {
+  second();
+  first();
+}
+
+if (a) {
+  first();
+  second();
+} else {
+  first();
+  third();
+}
+
+if (a) {
+  first();
+  second();
+} else {
+  first();
+}
+
+/*
+ * NOK SWITCH
+ */
+
+switch (a) {
+  case 1:
+    first();
+    second();
+    break;
+  default:
+    // [96:2-99:13] {{This case's code block is the same as the block for the case on line 92.}}
+    first();
+    second();
+}
+
+switch (a) {
+  case 1:
+    first();
+    second();
+    break;
+  case 2:
+    // [107:2-111:10] {{This case's code block is the same as the block for the case on line 103.}}
+    first();
+    second();
+    break;
+}
+switch (a) {
+  case 1:
+    first();
+    first();
+    break;
+  case 2:
+    second();
+    second();
+    break;
+  case 3:
+    // [122:2-126:10] {{This case's code block is the same as the block for the case on line 114.}}
+    first();
+    first();
+    break;
+}
+
+/*
+ * OK SWITCH
+ */
+
+// ok, small
+(function() {
+  switch (a) {
+    case 1:
+      return first();
+    default:
+      return first();
+  }
+})();
+
+switch (a) {
+  case 1: {
+    // comment
+    break;
+  }
+  case 2: {
+    // comment
+    break;
+  }
+}
+
+switch (a) {
+  case 1:
+    first();
+    second();
+    break;
+  default:
+    second();
+    first();
+}
+
+switch (a) {
+  case 1:
+    first();
+    second();
+    break;
+  case 2:
+    third();
+}
