a320873dc838bf99c6fd5204a241125de5b4fdf3
Stas Vilchik <stas.vilchik@sonarsource.com>
2017-07-28 17:22:48 +0200

    update no-self-assignment (S1656) to raise issues on "a = a.reverse()"

--- tests/rules/noSelfAssignmentRule/noSelfAssignmentRule.lint.ts
+++ tests/rules/noSelfAssignmentRule/noSelfAssignmentRule.lint.ts
@@ -27,3 +27,11 @@ x = x + x;
 
 while (x = x) {}
 //     ^^^^^       {{Remove or correct this useless self-assignment.}}
+
+let arr = [];
+
+  arr = arr.reverse();
+//^^^^^^^^^^^^^^^^^^^  {{Remove or correct this useless self-assignment.}}
+
+let notArray: any;
+notArray = notArray.reverse();
