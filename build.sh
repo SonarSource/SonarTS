b56d99380295aaf08a24394d3bfca51f2f8b581d
Elena Vilchik <elena.vilchik@sonarsource.com>
2017-10-13 11:17:14 +0200

    Add build script for entire project

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd sonarts-core
+
+yarn build
+yarn test
+yarn license-check
+npm pack
+
+cd ..
+
+cd sonarts-sq-plugin
+
+cd sonar-typescript-plugin/sonarts-core
+npm install ../../../sonarts-core/tslint-sonarts-*.tgz
+cd ../../
+
+mvn clean install
+
+cd ..
+
