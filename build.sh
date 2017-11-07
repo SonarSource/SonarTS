767e258d737eb1990fe61f7dd24a60e9d880ad0c
Tibor Blenessy <tibor.blenessy@sonarsource.com>
2017-11-07 09:47:51 +0100

    Install typescript and minor changes

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+set -eou
 
 #install npm
 node_home=$(pwd)/node-v8.9.0-win-x64
+node_archive=node.7z
 if [ ! -d "$node_home" ]; then
   echo "=== Install Node.js ===";
-  curl --insecure --silent --show-error -o node.7z https://nodejs.org/dist/v8.9.0/node-v8.9.0-win-x64.7z;
-  7z x node.7z;
-  rm node.zip;
+  curl --insecure --silent --show-error -o $node_archive https://nodejs.org/dist/v8.9.0/node-v8.9.0-win-x64.7z;
+  7z x $node_archive;
+  rm $node_archive;
 fi
 
 chmod 755 $node_home/node;
@@ -18,10 +20,9 @@ node -v
 
 #install yarn
 echo "=== Install yarn ===";
-npm install -g yarn;
-yarn -v
+npm install -g yarn typescript ;
 
-set -e
+yarn -v
 
 cd sonarts-core
 
