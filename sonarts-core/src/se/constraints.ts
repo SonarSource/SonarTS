d9e553fa4b96dfee569d44117f1a832201e16209
Stas Vilchik <stas.vilchik@sonarsource.com>
2017-11-14 14:38:55 +0100

    fix expressions.test.ts

---
+++ sonarts-core/src/se/constraints.ts
@@ -0,0 +1,43 @@
+/*
+ * SonarTS
+ * Copyright (C) 2017-2017 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+export enum ConstraintKind {
+  Truthy,
+  Falsy,
+}
+
+export interface Constraint {
+  kind: ConstraintKind;
+}
+
+export interface TruthyConstraint extends Constraint {
+  kind: ConstraintKind.Truthy;
+}
+
+export interface FalsyConstraint extends Constraint {
+  kind: ConstraintKind.Falsy;
+}
+
+export function isTruthyConstraint(constraint: Constraint): constraint is TruthyConstraint {
+  return constraint.kind === ConstraintKind.Truthy;
+}
+
+export function isFalsyConstraint(constraint: Constraint): constraint is FalsyConstraint {
+  return constraint.kind === ConstraintKind.Falsy;
+}
