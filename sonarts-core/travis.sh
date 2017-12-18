fd5dc5975f7dd22c9ab593e2d8f0e5262fc6f9a4
Stas Vilchik <stas.vilchik@sonarsource.com>
2017-12-18 14:24:42 +0100

    test all supported TS versions

--- sonarts-core/travis.sh
+++ sonarts-core/travis.sh
@@ -3,7 +3,10 @@ set -euo pipefail
 
 cd sonarts-core
 yarn build
+# run tests with the current TS version to generate test coverage
 yarn test -- --coverage
+# run tests with different TS versions, but the current one
+yarn test-ts-versions
 yarn license-check
 npm pack
 cd ..
