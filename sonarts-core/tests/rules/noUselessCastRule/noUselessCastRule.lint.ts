e539ffdca16b6bb89c0ec1adf7e5b036bfc31c2b
Elena Vilchik <elena.vilchik@sonarsource.com>
2018-01-02 12:43:29 +0100

    Extend no-useless-cast/S4325: support cast to super type (#411)

--- sonarts-core/tests/rules/noUselessCastRule/noUselessCastRule.lint.ts
+++ sonarts-core/tests/rules/noUselessCastRule/noUselessCastRule.lint.ts
@@ -52,3 +52,25 @@ null!;
 class UserName {
   name: string;
 }
+
+class SuperClass {}
+
+class SubClass extends SuperClass {}
+class SubSubClass extends SubClass {}
+
+interface SuperInterface {}
+interface SubInterface extends SuperInterface {}
+
+class ImplementationClass implements SuperInterface {}
+
+function parentType(p1: SubClass, p2: SubInterface, p3: ImplementationClass, p4: SubSubClass, p5: SuperClass) {
+  p1 as SuperClass;
+//^^^^^^^^^^^^^^^^ {{Remove this unnecessary cast.}}
+  p2 as SuperInterface;
+//^^^^^^^^^^^^^^^^^^^^ {{Remove this unnecessary cast.}}
+  p4 as SuperClass;
+//^^^^^^^^^^^^^^^^ {{Remove this unnecessary cast.}}
+
+  p3 as SuperInterface; // OK, FN
+  p5 as SubClass; // OK
+}
