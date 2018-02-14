33bb4ada008d5c80d9a50f71b97d70b7f5cf2e47
Tibor Blenessy <tibor.blenessy@sonarsource.com>
2018-02-14 17:50:30 +0100

    Remove Unicode 8194 "en space" character from license notice (#526)

--- sonarts-core/tests/runner/cpd.test.ts
+++ sonarts-core/tests/runner/cpd.test.ts
@@ -10,12 +10,12 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 import getCpdTokens, { CpdToken } from "../../src/runner/cpd";
 import { parseString } from "../../src/utils/parser";
