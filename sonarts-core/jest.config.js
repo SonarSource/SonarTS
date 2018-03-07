5be9d9339da7a2653de8976f74fc9b672af29b16
Elena <elena.vilchik@sonarsource.com>
2018-03-07 16:50:40 +0100

    Add unit test report to sonar analysis

--- sonarts-core/jest.config.js
+++ sonarts-core/jest.config.js
@@ -6,6 +6,7 @@ module.exports = {
   },
   mapCoverage: true,
   moduleFileExtensions: ["js", "ts"],
+  testResultsProcessor: "jest-sonar-reporter",
   transform: {
     "^.+\\.ts$": "<rootDir>/node_modules/ts-jest/preprocessor.js",
   },
