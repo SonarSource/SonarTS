f7feeebebdd1e4498c448acf90c8135ec81cad83
Andrea Guarino <andrea.guarino@sonarsource.com>
2018-05-14 11:44:25 +0200

    avoid using TypedSonarVisitor

--- sonarts-core/src/rules/noDuplicateInCompositeRule.ts
+++ sonarts-core/src/rules/noDuplicateInCompositeRule.ts
@@ -20,10 +20,10 @@
 import * as tslint from "tslint";
 import * as ts from "typescript";
 import { SonarRuleMetaData } from "../sonarRule";
-import { TypedSonarRuleVisitor } from "../utils/sonarAnalysis";
+import { SonarRuleVisitor } from "../utils/sonarAnalysis";
 import areEquivalent from "../utils/areEquivalent";
 
-export class Rule extends tslint.Rules.TypedRule {
+export class Rule extends tslint.Rules.AbstractRule {
   public static metadata: SonarRuleMetaData = {
     ruleName: "no-duplicate-in-composite",
     description: "Composite types should not be defined with duplicated elements",
@@ -37,12 +37,12 @@ export class Rule extends tslint.Rules.TypedRule {
 
   public static MESSAGE = "Remove this duplicated type.";
 
-  public applyWithProgram(sourceFile: ts.SourceFile, program: ts.Program): tslint.RuleFailure[] {
-    return new Visitor(this.getOptions().ruleName, program).visit(sourceFile).getIssues();
+  public apply(sourceFile: ts.SourceFile): tslint.RuleFailure[] {
+    return new Visitor(this.getOptions().ruleName).visit(sourceFile).getIssues();
   }
 }
 
-class Visitor extends TypedSonarRuleVisitor {
+class Visitor extends SonarRuleVisitor {
   public visitIntersectionTypeNode(node: ts.IntersectionTypeNode) {
     this.checkDuplication(node);
     super.visitIntersectionTypeNode(node);
