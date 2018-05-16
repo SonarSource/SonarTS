72dee884fdf9fb6f96548d0c192008d4e9345e0a
Andrea Guarino <andrea.guarino@sonarsource.com>
2018-05-16 09:24:56 +0200

    fix after review

--- sonarts-core/tests/rules/noInMisuseRule/noInMisuseRule.lint.ts
+++ sonarts-core/tests/rules/noInMisuseRule/noInMisuseRule.lint.ts
@@ -1,35 +1,29 @@
 function nokFunc1() {
   let arr = ["a", "b", "c"];
-
-  let expectedValue = "b";
-  if (expectedValue in arr) {
-//    ^^^^^^^^^^^^^^^^^^^^    {{Use "indexOf" or "includes" (available from ES2016) instead.}}
-    return expectedValue + " found in the array";
+  let expectedValue1 = "1";
+  let expectedValue2 = "b";
+  if (expectedValue1 in arr) {
+  //  ^^^^^^^^^^^^^^^^^^^^^    {{Use "indexOf" or "includes" (available from ES2016) instead.}}
+    console.log("OK");
+  }
+  if (expectedValue2 in arr) {
+//    ^^^^^^^^^^^^^^^^^^^^^    {{Use "indexOf" or "includes" (available from ES2016) instead.}}
+    return 1;
   } else {
-    return expectedValue + " not found";
+    return 2;
   }
 }
 
-function nokFunc2() {
-  let arr = ["a", "b", "c"];
-
-  let expectedValue = "1"; // index #1 is corresponding to the value "b"
-  if (expectedValue in arr) {
-//    ^^^^^^^^^^^^^^^^^^^^    {{Use "indexOf" or "includes" (available from ES2016) instead.}}
-    return expectedValue + " found in the array";
-  } else {
-    return expectedValue + " not found";
-  }
-}
-
-
+// An erroneous way to search for an object property
 function nokFunc3(a: any) {
+  // testing "in" operator on a ternary operator
   const result = "car" in Object.keys(a) ? "something" : "someething else";
   //             ^^^^^^^^^^^^^^^^^^^^^^^ {{Use "indexOf" or "includes" (available from ES2016) instead.}}
   return result;
 }
 
-function nokContains(array: any[], elem: any) {
+// An erroneous ES2016 "includes" implementation
+function nokIncludes(array: any[], elem: any) {
   const result = elem in array;
   //             ^^^^^^^^^^^^^ {{Use "indexOf" or "includes" (available from ES2016) instead.}}
   return result;
