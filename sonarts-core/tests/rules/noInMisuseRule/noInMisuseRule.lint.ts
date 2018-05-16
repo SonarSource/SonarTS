e01efd3880efdcd9ab52779cb2e3ab414ba75233
Andrea Guarino <andrea.guarino@sonarsource.com>
2018-05-16 15:31:34 +0200

    improve tests

--- sonarts-core/tests/rules/noInMisuseRule/noInMisuseRule.lint.ts
+++ sonarts-core/tests/rules/noInMisuseRule/noInMisuseRule.lint.ts
@@ -1,48 +1,38 @@
-function nokFunc1() {
-  let arr = ["a", "b", "c"];
-  let expectedValue1 = "1";
-  let expectedValue2 = "b";
-  if (expectedValue1 in arr) {
-  //  ^^^^^^^^^^^^^^^^^^^^^    {{Use "indexOf" or "includes" (available from ES2016) instead.}}
-    console.log("OK");
-  }
-  if (expectedValue2 in arr) {
-//    ^^^^^^^^^^^^^^^^^^^^^    {{Use "indexOf" or "includes" (available from ES2016) instead.}}
-    return 1;
-  } else {
-    return 2;
-  }
+let arr = ["a", "b", "c"];
+
+  "1" in arr;
+//^^^^^^^^^^ {{Use "indexOf" or "includes" (available from ES2016) instead.}}
+  1 in arr;
+//^^^^^^^^ {{Use "indexOf" or "includes" (available from ES2016) instead.}}
+  "b" in arr;
+//^^^^^^^^^^ {{Use "indexOf" or "includes" (available from ES2016) instead.}}
+
+
+// in different contexts
+const result = "car" in arr ? "something" : "someething else";
+//             ^^^^^^^^^^^^ {{Use "indexOf" or "includes" (available from ES2016) instead.}}
+foo("car" in arr);
+//  ^^^^^^^^^^^^ {{Use "indexOf" or "includes" (available from ES2016) instead.}}
+if ("car" in arr) {}
+//  ^^^^^^^^^^^^ {{Use "indexOf" or "includes" (available from ES2016) instead.}}
+
+
+// to check the property of an object do this
+"car" in { "car" : 1};
+// and not this
+  "car" in Object.keys({ "car": 1 });
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ {{Use "indexOf" or "includes" (available from ES2016) instead.}}
+
+function erroneousIncludesES2016(array: any[], elem: any) {
+  return elem in array;
+  //     ^^^^^^^^^^^^^ {{Use "indexOf" or "includes" (available from ES2016) instead.}}
 }
 
-// An erroneous way to search for an object property
-function nokFunc3(a: any) {
-  // testing "in" operator on a ternary operator
-  const result = "car" in Object.keys(a) ? "something" : "someething else";
-  //             ^^^^^^^^^^^^^^^^^^^^^^^ {{Use "indexOf" or "includes" (available from ES2016) instead.}}
-  return result;
-}
 
-// An erroneous ES2016 "includes" implementation
-function nokIncludes(array: any[], elem: any) {
-  const result = elem in array;
-  //             ^^^^^^^^^^^^^ {{Use "indexOf" or "includes" (available from ES2016) instead.}}
-  return result;
-}
+const dict = {a: 1, b: 2, c: 3};
+"a" in dict;  // OK on objects
 
-function okFunc(a: string) {
-  const dict = {
-    a: 1,
-    b: 2,
-    c: 3,
-  };
-  if (a in dict) {
-    return "Something";
-  }
-  return "Something else";
-}
-
-// Shouldn't be applied on array-like objects
-function okFunc1(a: any, b: any) {
+function okOnArrayLikeObjects(a: any, b: any) {
   let key = "1";
   if (key in arguments) {
     return "Something";
