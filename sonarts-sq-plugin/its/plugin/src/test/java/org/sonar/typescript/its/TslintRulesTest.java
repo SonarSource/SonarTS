581e0479473ca2c085f42d2cb23a81e775a93202
Andrea Guarino <andrea.guarino@sonarsource.com>
2018-08-29 17:34:26 +0200

    Add IT for no-implicit-dependencies rule

--- sonarts-sq-plugin/its/plugin/src/test/java/org/sonar/typescript/its/TslintRulesTest.java
+++ sonarts-sq-plugin/its/plugin/src/test/java/org/sonar/typescript/its/TslintRulesTest.java
@@ -23,6 +23,7 @@ import com.google.common.collect.ImmutableList;
 import com.sonar.orchestrator.Orchestrator;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.sonarqube.ws.Issues.Issue;
@@ -39,19 +40,29 @@ public class TslintRulesTest {
   public static Orchestrator orchestrator = Tests.ORCHESTRATOR;
 
   @Test
-  public void should_avoid_escape_in_quote_rule() {
-    assertIssueLines("S1441", "profile-single-quotes", ImmutableList.of(1));
-    assertIssueLines("S1441", "profile-double-quotes", ImmutableList.of(3));
+  public void S1441_should_avoid_escape_in_quote_rule() {
+    List<Issue> issueList = getIssues("S1441", "profile-single-quotes");
+    assertThat(issueList).extracting("line").containsExactlyElementsOf(ImmutableList.of(1));
+
+    issueList = getIssues("S1441", "profile-double-quotes");
+    assertThat(issueList).extracting("line").containsExactlyElementsOf(ImmutableList.of(3));
   }
 
-  private void assertIssueLines(String ruleKey, String profileKey, List<Integer> expectedLines) {
+  @Test
+  public void S4328_should_consider_peer_dependencies() {
+    List<Issue> issueList = getIssues("S4328", "profile-no-implicit-dependencies");
+    assertThat(issueList).extracting("line").doesNotContainAnyElementsOf(ImmutableList.of(1));
+  }
+
+  private List<Issue> getIssues(String ruleKey, String profileKey) {
     orchestrator.resetData();
     orchestrator.executeBuild(Tests.createScanner("projects/tslint-rules-test-project", PROJECT_KEY).setProfile(profileKey));
 
     SearchRequest request = new SearchRequest();
     request.setComponentKeys(Collections.singletonList(PROJECT_KEY)).setRules(ImmutableList.of("typescript:" + ruleKey));
-    List<Issue> issuesList = newWsClient().issues().search(request).getIssuesList();
-    assertThat(issuesList).extracting("line").containsExactlyElementsOf(expectedLines);
+    return newWsClient().issues().search(request).getIssuesList()
+      .stream().filter(issue -> issue.getComponent().endsWith(ruleKey + ".ts"))
+      .collect(Collectors.toList());
   }
 
 }
