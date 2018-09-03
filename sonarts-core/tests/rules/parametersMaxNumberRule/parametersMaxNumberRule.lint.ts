c9f1b5be94df89d83ff37c9da04a937cedc85474
Andrea Guarino <andrea.guarino@sonarsource.com>
2018-09-03 19:07:01 +0200

    Rule parameters-max-number: allow exception for parameter properties

--- sonarts-core/tests/rules/parametersMaxNumberRule/parametersMaxNumberRule.lint.ts
+++ sonarts-core/tests/rules/parametersMaxNumberRule/parametersMaxNumberRule.lint.ts
@@ -4,3 +4,35 @@
 }
 
 function ok(p1: number, p2: number, p3: number, p4: number, p5: number, p6: number, p7: number) { }
+
+
+class Foo {
+  // OK, class constructor with parameter properties are excluded
+  public constructor(
+    private store: Store,
+    private scuttlebot: ScuttlebotService,
+    private electron: ElectronService,
+    private router: Router,
+    private helper: HelperService,
+    public elementRef: ElementRef,
+    private _hotkeysService: HotkeysService,
+    private _scrollService: ScrollToService,
+    private changeDetectorRef: ChangeDetectorRef,
+  ) { }
+}
+
+class Bar {
+  // OK, class constructor with parameter properties are excluded
+  public constructor(
+//       ^^^^^^^^^^^ {{This function has 8 parameters, which is greater than the 7 authorized.}}
+    private store: Store,
+    scuttlebot: ScuttlebotService,
+    electron: ElectronService,
+    router: Router,
+    helper: HelperService,
+    elementRef: ElementRef,
+    _hotkeysService: HotkeysService,
+    _scrollService: ScrollToService,
+    changeDetectorRef: ChangeDetectorRef,
+  ) { }
+}
