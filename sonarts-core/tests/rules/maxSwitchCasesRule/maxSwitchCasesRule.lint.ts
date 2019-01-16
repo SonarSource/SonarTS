5012a7f4a839db192f5823cea36382f82b3d73a4
Andrea Guarino <andrea.guarino@sonarsource.com>
2019-01-16 14:10:40 +0100

    addition to max-switch-cases

--- sonarts-core/tests/rules/maxSwitchCasesRule/maxSwitchCasesRule.lint.ts
+++ sonarts-core/tests/rules/maxSwitchCasesRule/maxSwitchCasesRule.lint.ts
@@ -1,8 +1,36 @@
-switch(i) {
-  case 1:
-    f();
-  case 2:
-    g();
+  switch(i) {
+//^^^^^^{{Reduce the number of non-empty switch cases from 31 to at most 30.}}
+  case 1: f();
+  case 2: f();
+  case 3: f();
+  case 4: f();
+  case 5: f();
+  case 6: f();
+  case 7: f();
+  case 8: f();
+  case 9: f();
+  case 10: f();
+  case 11: f();
+  case 12: f();
+  case 13: f();
+  case 14: f();
+  case 15: f();
+  case 16: f();
+  case 17: f();
+  case 18: f();
+  case 19: f();
+  case 20: f();
+  case 21: f();
+  case 22: f();
+  case 23: f();
+  case 24: f();
+  case 25: f();
+  case 26: f();
+  case 27: f();
+  case 28: f();
+  case 29: f();
+  case 30: f();
+  case 31: f();
 }
 
 switch(i) {
@@ -10,16 +38,6 @@ switch(i) {
     f();
   case 2:
     g();
-  default:
-    console.log("foo");
-}
-
-switch(i) {
-  case 1:
-  case 2:
-    g();
-  case 3:
-    console.log("foo");
 }
 
 switch(i) {}
--- sonarts-core/tests/rules/maxSwitchCasesRule/maxSwitchCasesRule.test.ts
+++ sonarts-core/tests/rules/maxSwitchCasesRule/maxSwitchCasesRule.test.ts
@@ -18,8 +18,12 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 import { Rule } from "../../../src/rules/maxSwitchCasesRule";
-import runRule from "../../runRule";
+import runRule, { runRuleWithLintFile } from "../../runRule";
 
 it("raises error", () => {
   runRule(Rule, __filename);
+});
+
+it("raises errors when set to 3", () => {
+  runRuleWithLintFile(Rule, "maxSwitchCasesRule", "maxSwitchCasesRule3Elements", 3);
 });
